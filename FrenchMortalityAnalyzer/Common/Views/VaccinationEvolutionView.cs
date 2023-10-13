32ecbaff337b91af4a753f708480953f6ca6e69c
devjnh <142326699+devjnh@users.noreply.github.com>
2023-10-13 19:25:50 +0200

    Improved rolling period charts min/max adjustment

--- FrenchMortalityAnalyzer/Common/Views/VaccinationEvolutionView.cs
+++ FrenchMortalityAnalyzer/Common/Views/VaccinationEvolutionView.cs
@@ -121,13 +121,30 @@ namespace MortalityAnalyzer.Views
                 vaccinationSerie.Header = $"{MortalityEvolution.Injections} injections";
                 vaxChart.XAxis.Crosses = eCrosses.Min;
                 evolutionChart.UseSecondaryAxis = true;
-                evolutionChart.YAxis.MinValue = MortalityEvolution.MinExcess;
-                evolutionChart.YAxis.MaxValue = MortalityEvolution.MaxExcess;
-                vaxChart.YAxis.MinValue = MortalityEvolution.MaxInjections * MortalityEvolution.MinExcess / MortalityEvolution.MaxExcess * 1.4;
-                vaxChart.YAxis.MaxValue = MortalityEvolution.MaxInjections * 1.4;
+                AdjustMinMax(evolutionChart, vaxChart);
             }
             evolutionChart.SetPosition(3 + startChartRow, 0, 7, 0);
             evolutionChart.SetSize(900, 500);
+        }
+
+        private void AdjustMinMax(ExcelChart evolutionChart, ExcelChart vaxChart)
+        {
+            if (MortalityEvolution.MinExcess < 0)
+            {
+                double resolution = MortalityAnalyzer.MortalityEvolution.GetHistogramResolution(-MortalityEvolution.MinExcess, 20, true);
+                evolutionChart.YAxis.MinValue = Round(MortalityEvolution.MinExcess, resolution);
+                evolutionChart.YAxis.MaxValue = MortalityEvolution.MaxExcess;
+                double minInjections = MortalityEvolution.MaxInjections * evolutionChart.YAxis.MinValue.Value / MortalityEvolution.MaxExcess * 1.4;
+                double maxInjections = MortalityEvolution.MaxInjections * 1.4;
+                resolution = MortalityAnalyzer.MortalityEvolution.GetHistogramResolution(-minInjections, 20, true);
+                vaxChart.YAxis.MinValue = Round(minInjections, resolution);
+                vaxChart.YAxis.MaxValue = maxInjections;
+            }
+        }
+
+        double Round(double value, double resolution)
+        {
+            return Math.Round(value / resolution, MidpointRounding.AwayFromZero) * resolution;    
         }
         public void Save()
         {
