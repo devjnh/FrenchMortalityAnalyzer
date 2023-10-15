95ffae251ab32dd5b71de3581a36f0607fdca1ee
devjnh <142326699+devjnh@users.noreply.github.com>
2023-10-15 19:04:56 +0200

    Moved Options class on Common folder

---
+++ FrenchMortalityAnalyzer/Common/Options.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Linq;
+namespace MortalityAnalyzer
+{
+    public class Options
+    {
+        [Option('f', "folder", Required = false, HelpText = "Data folder")]
+        public string Folder { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+        [Option('o', "OutputFile", Required = false, HelpText = "Output spreadsheet file (.xlsx)")]
+        public string OutputFile { get; set; } = "FrenchMortality.xlsx";
+    }
+}
--- FrenchMortalityAnalyzer/Options.cs
+++ FrenchMortalityAnalyzer/Options.cs
@@ -8,13 +8,6 @@ using System.Threading.Tasks;
 
 namespace MortalityAnalyzer
 {
-    public class Options
-    {
-        [Option('f', "folder", Required = false, HelpText = "Data folder")]
-        public string Folder { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "Data");
-        [Option('o', "OutputFile", Required = false, HelpText = "Output spreadsheet file (.xlsx)")]
-        public string OutputFile { get; set; } = "FrenchMortality.xlsx";
-    }
     [Verb("init", HelpText = "Parse and insert data in the database")]
     public class InitOptions : Options
     {
